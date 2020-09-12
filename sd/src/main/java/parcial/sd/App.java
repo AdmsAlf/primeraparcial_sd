bd80919a2e62ac093c589daa10cc77c5c1b7e412
Adams Alfonso <60947558+AdmsAlf@users.noreply.github.com>
2020-09-12 09:46:49 -0400

    proyecto creado

---
+++ sd/src/main/java/parcial/sd/App.java
@@ -0,0 +1,13 @@
+package parcial.sd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
